7e86f5e4b8045d7be70f827cd7850646c6872a62
ROllerozxa <rollerozxa@voxelmanip.se>
2024-01-08 20:18:48 +0100

    Initial Commit

---
+++ metadata.php
@@ -0,0 +1,52 @@
+<?php
+
+$newsMetadata = [
+	1 => [
+		'title' => "Welcome!",
+		'time' => 1609255844,
+	],
+	2 => [
+		'title' => "The principia-web forum has launched! (& More!)",
+		'time' => 1625929570,
+	],
+	3 => [
+		'title' => "Dark Mode is back!",
+		'time' => 1626111981,
+	],
+	4 => [
+		'title' => "Friendship ended with APACHE, now NGINX is my best friend",
+		'time' => 1634063010,
+	],
+	5 => [
+		'title' => "principia-web is moving!",
+		'time' => 1648289019,
+	],
+	6 => [
+		'title' => "The Principia source code has been released!",
+		'time' => 1660239961,
+	],
+	7 => [
+		'title' => "principia-web turns 2!",
+		'time' => 1672054518,
+	],
+	8 => [
+		'title' => "Principia 1.5.2 Beta builds are here!",
+		'time' => 1672525437,
+	],
+	9 => [
+		'title' => "Principia 1.5.2 Beta build 2023-03-10",
+		'time' => 1678474397,
+	],
+	10 => [
+		'title' => "The new Principia Official Community Site Archive",
+		'time' => 1688975568,
+	],
+	11 => [
+		'title' => "Follow Principia on Mastodon!",
+		'time' => 1694336846,
+	],
+	12 => [
+		'title' => "Principia 1.5.2 Beta build 2023-12-26",
+		'time' => 1703606346,
+	],
+];
